d9dbf2cb3c453c7448af76ee8135cd6354b42e2a
gstrenge <gstrenge01@gmail.com>
2019-06-18 10:09:25 -0400

    Last Minute Changes. Fingers Crossed

--- handDetector/handDetector.py
+++ handDetector/handDetector.py
@@ -18,6 +18,9 @@ rightHandPin = 35
 makeSafePin = 38
 dropGuardPin = 40
 
+commsPin = 36
+
+
 
 
 
@@ -53,6 +56,7 @@ def main():
 	GPIO.setup(rightHandPin, GPIO.OUT)
 	GPIO.setup(makeSafePin, GPIO.OUT)
 	GPIO.setup(dropGuardPin, GPIO.OUT)
+	GPIO.setup(commsPin, GPIO.IN)
 
 	GPIO.output(redLEDPin, False)
 	GPIO.output(yellowLEDPin, False)
@@ -118,260 +122,263 @@ def main():
 
 		while True:
 
-			#----------------Getting Frame of Video Feed--------------------
-			#ret, img = cap.read()
-			img, frame_id = l.capture()
+			if checkPin(commsPin) == True:
+				#----------------Getting Frame of Video Feed--------------------
+				#ret, img = cap.read()
+				img, frame_id = l.capture()
 
-			if frame_id == previousId:
-				print ("Identical")
-				continue
+				if frame_id == previousId:
+					print ("Identical")
+					continue
 
-			cv2.normalize(img, img, 0, 65535, cv2.NORM_MINMAX) # extend contrast
-			np.right_shift(img, 8, img) # fit data into 8 bits
-			img = cv2.resize(img, None, fx = scaleFactorOverall, fy = scaleFactorOverall)
+				cv2.normalize(img, img, 0, 65535, cv2.NORM_MINMAX) # extend contrast
+				np.right_shift(img, 8, img) # fit data into 8 bits
+				img = cv2.resize(img, None, fx = scaleFactorOverall, fy = scaleFactorOverall)
 
-			img = np.uint8(img)
+				img = np.uint8(img)
 
-			#img = cv2.resize(img, (0,0), fx=5, fy=5)
+				#img = cv2.resize(img, (0,0), fx=5, fy=5)
 
-			greenLEDPinBool = True
-			redLEDPinBool = False
-			yellowLEDPinBool = False
-			leftHandPinBool = False
-			rightHandPinBool = False
-			makeSafePinBool = False
-			dropGuardPinBool = False
-
-
-			#---------------Frame Rate Counter---------------------
-			if frameTime >= 1:
-				print(frameCounter)
-				frameCounter = 0
-				frameTime = 0
+				greenLEDPinBool = True
+				redLEDPinBool = False
+				yellowLEDPinBool = False
+				leftHandPinBool = False
+				rightHandPinBool = False
+				makeSafePinBool = False
+				dropGuardPinBool = False
 
 
-			#-----------Intializing Booleans that indicate Danger--------------
-			#If hand is in the danger Rectange
-			inRect = False
-
-			#If both of these danger indicators agree (are True), then the blade guard will drop
-			dangerDifference = False
-			dangerVelocity = False
+				#---------------Frame Rate Counter---------------------
+				if frameTime >= 1:
+					print(frameCounter)
+					frameCounter = 0
+					frameTime = 0
 
 
+				#-----------Intializing Booleans that indicate Danger--------------
+				#If hand is in the danger Rectange
+				inRect = False
 
-			#----------------------------------------------------------------
-			#-----------------Difference (Danger Check #1)-------------------
-			#----------------------------------------------------------------
-			#Getting Grayscale Version of Frame
-			#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-			gray = img
-
-			#Blurring Grayscale Image for better processing
-			#grayBlurred = cv2.bilateralFilter(gray,9,75,75)
-			grayBlurred = gray
-
-
-			#Adding Blurred Gray Image to difference checker Array
-			frameArray.append(grayBlurred)
-
-			#Finding the difference between the two frames (Looking for change)
+				#If both of these danger indicators agree (are True), then the blade guard will drop
+				dangerDifference = False
+				dangerVelocity = False
 
 
 
-			diff = cv2.absdiff(frameArray[0], frameArray[1])
+				#----------------------------------------------------------------
+				#-----------------Difference (Danger Check #1)-------------------
+				#----------------------------------------------------------------
+				#Getting Grayscale Version of Frame
+				#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-			#Downscaling for performance
-			diffSmall = cv2.resize(diff, None, fx = scaleFactor, fy = scaleFactor)
+				gray = img
 
-			#Adding upwhite value in each pixel (shows movement)
-			sum = 0
-
-			for pixel in np.nditer(diffSmall, order='C'):
-
-				#print(pixel)
-
-				sum += pixel
+				#Blurring Grayscale Image for better processing
+				#grayBlurred = cv2.bilateralFilter(gray,9,75,75)
+				grayBlurred = gray
 
 
-			#sum = cv2.sumElems(diffSmall)
-			#sum = frame_id
+				#Adding Blurred Gray Image to difference checker Array
+				frameArray.append(grayBlurred)
 
-
-			#print("Sum: " + str(sum))
+				#Finding the difference between the two frames (Looking for change)
 
 
 
-			#If there is enough movement, there is danger (60000 is just a tested value that works)
-			if sum > 60000:
-				#dangerDifference = True
-				redLEDPinBool = True
-				dropGuardPinBool = True
-				makeSafePinBool = True
+				diff = cv2.absdiff(frameArray[0], frameArray[1])
+
+				#Downscaling for performance
+				diffSmall = cv2.resize(diff, None, fx = scaleFactor, fy = scaleFactor)
+
+				#Adding upwhite value in each pixel (shows movement)
+				sum = 0
+
+				for pixel in np.nditer(diffSmall, order='C'):
+
+					#print(pixel)
+
+					sum += pixel
+
+
+				#sum = cv2.sumElems(diffSmall)
+				#sum = frame_id
+
+
+				#print("Sum: " + str(sum))
+
+
+
+				#If there is enough movement, there is danger (60000 is just a tested value that works)
+				if sum > 60000:
+					#dangerDifference = True
+					redLEDPinBool = True
+					dropGuardPinBool = True
+					makeSafePinBool = True
+					#GPIO.output(makeSafePin, makeSafePinBool)
+					#GPIO.output(dropGuardPin, dropGuardPinBool)
+
+
+
+
+
+
+
+
+
+
+
+
+				#Thresholding to only bright objects -------------------------------------------To Add Infared Stuff Here
+
+
+				#Change 100 to average pixel color?
+				thresholdValue = np.mean(grayBlurred)
+				print(thresholdValue)
+				ret,thresh1 = cv2.threshold(grayBlurred,100,255,cv2.THRESH_BINARY)
+
+				#Using Special Threshold Method
+				#ret2, thresh2 = cv2.threshold(thresh1, 0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+				thresh2 = thresh1
+
+				#Erode/Dilate (Look at cv2.MORPH_OPEN/ClOSE on docs to see which goes first)
+				thresh3 = cv2.morphologyEx(thresh2, cv2.MORPH_OPEN, kernel)
+
+
+				#-------------------Calculating Center of Mass-----------------
+				M = cv2.moments(thresh3)
+
+				if(M['m00'] != 0):
+
+					centerx = int(M['m10']/M['m00'])
+					centery = int(M['m01']/M['m00'])
+
+				else:
+					centerx = 0
+					centery = 0
+
+				#-----------------Calculating the velocity-------------------
+				#Calculating the change in time since the last frame
+				timeFinal = time.time()
+				dt = timeFinal - timeInitial
+
+
+				#----------------Updating Velocity/Future Position--------------------
+				#Updating position/velocity
+				center.update(centerx, centery, dt)
+
+				#Based on how fast the users hands are moving, the danger rectangle increases/decreases in size
+
+
+				extraSizeToRecty = int((center.vy))
+				if extraSizeToRecty < 0:
+					extraSizeToRecty = 0
+				extraSizeToRectx = int(abs(center.vx**2))/5
+
+				#Prediciting future position
+				futurePos = center.futurePosition(.2)
+
+
+				#-----------------Drawing Velocity/Position on Screen-------------------
+				cv2.circle(img, (centerx, centery), 10, (0,0,255))
+				cv2.circle(img, (futurePos), 10, (0,0,255))
+				#cv2.arrowedLine(img, (centerx, centery), (futurePos[0], futurePos[1]), (0,0,255), 3)
+
+
+				#----------------------------------------------------------------
+				#----------Velocity/Danger Area (Danger Check #2)----------------
+				#----------------------------------------------------------------
+
+				#Finding Contours in black and white image
+				contours, hierarchy = cv2.findContours(thresh3, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)   #NONE
+
+				#Initiating Hull Array
+				hull = []
+
+				#Filling Convex Hull array with points
+
+				for i in range(len(contours)):
+					hull.append(cv2.convexHull(contours[i], False))
+
+				if len(contours) == 1:
+					leftHandPinBool = True
+				elif len(contours) >= 2:
+					leftHandPinBool = True
+					rightHandPinBool = True
+
+				for i in range(len(contours)):
+					#Drawing Contours
+					area = cv2.contourArea(contours[i])
+					if area < 500:
+						continue
+					cv2.drawContours(img, contours, i, (255,255,255), 1, 1, hierarchy)
+					#Drawing Hull
+					#cv2.drawContours(img, hull, i, (255,0,0), 3, 8)
+
+					for j in range(len(contours[i])):
+
+						if contours[i][j][0][1] > height - rectHeight - extraSizeToRecty:
+							if contours[i][j][0][0] > width/2 - rectWidth - extraSizeToRectx/2 and contours[i][j][0][0] < width/2 + rectWidth + extraSizeToRectx/2 :
+								inRect = True
+
+
+				#------------If the hand is in the Danger Rectangle, do action
+				if inRect:
+					color = (255,255,255)
+					dangerVelocity = True
+				else:
+					color = (255,255,255)
+
+
+				#Drawing Danger Rectangle
+				cv2.rectangle(img, (width/2-rectWidth - extraSizeToRectx/2 , height - rectHeight - extraSizeToRecty), (width/2+rectWidth + extraSizeToRectx/2 , height), color, 0)
+
+
+
+				#cv2.imshow("img", img)
+				large = cv2.resize(img, (0,0), fx=5, fy=5)
+				cv2.imshow("large", large)
+				#cv2.imshow("diff", diffSmall)
+
+				#Updating currentFrame to be LastFrame
+				frameArray[0] = frameArray[1]
+				frameArray.pop(1)
+
+				timeInitial = time.time()
+
+				if dangerDifference or dangerVelocity:
+					#print("Multi")
+					redLEDPinBool = True
+					greenLEDPinBool = False
+					dropGuardPinBool = True
+					makeSafePinBool = True
+
+					thread = Thread(target=playWarning)
+					thread.daemon = True
+					thread.start()
+
 				GPIO.output(makeSafePin, makeSafePinBool)
 				GPIO.output(dropGuardPin, dropGuardPinBool)
 
+				GPIO.output(redLEDPin, redLEDPinBool)
+				GPIO.output(greenLEDPin, greenLEDPinBool)
+				GPIO.output(yellowLEDPin, yellowLEDPinBool)
+				GPIO.output(leftHandPin, leftHandPinBool)
+				GPIO.output(rightHandPin, rightHandPinBool)
 
 
+				k = cv2.waitKey(45) & 0xff
+				if k == 27:
+					break
 
+				#Adding to Frame Counter
+				frameCounter += 1
+				frameTime += dt
 
-
-
-
-
-
-
-
-			#Thresholding to only bright objects -------------------------------------------To Add Infared Stuff Here
-
-
-			#Change 100 to average pixel color?
-			thresholdValue = np.mean(grayBlurred)
-			print(thresholdValue)
-			ret,thresh1 = cv2.threshold(grayBlurred,100,255,cv2.THRESH_BINARY)
-
-			#Using Special Threshold Method
-			#ret2, thresh2 = cv2.threshold(thresh1, 0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-			thresh2 = thresh1
-
-			#Erode/Dilate (Look at cv2.MORPH_OPEN/ClOSE on docs to see which goes first)
-			thresh3 = cv2.morphologyEx(thresh2, cv2.MORPH_OPEN, kernel)
-
-
-			#-------------------Calculating Center of Mass-----------------
-			M = cv2.moments(thresh3)
-
-			if(M['m00'] != 0):
-
-				centerx = int(M['m10']/M['m00'])
-				centery = int(M['m01']/M['m00'])
+				previousId = frame_id
 
 			else:
-				centerx = 0
-				centery = 0
-
-			#-----------------Calculating the velocity-------------------
-			#Calculating the change in time since the last frame
-			timeFinal = time.time()
-			dt = timeFinal - timeInitial
-
-
-			#----------------Updating Velocity/Future Position--------------------
-			#Updating position/velocity
-			center.update(centerx, centery, dt)
-
-			#Based on how fast the users hands are moving, the danger rectangle increases/decreases in size
-
-
-			extraSizeToRecty = int((center.vy))
-			if extraSizeToRecty < 0:
-				extraSizeToRecty = 0
-			extraSizeToRectx = int(abs(center.vx**2))/5
-
-			#Prediciting future position
-			futurePos = center.futurePosition(.2)
-
-
-			#-----------------Drawing Velocity/Position on Screen-------------------
-			cv2.circle(img, (centerx, centery), 10, (0,0,255))
-			cv2.circle(img, (futurePos), 10, (0,0,255))
-			#cv2.arrowedLine(img, (centerx, centery), (futurePos[0], futurePos[1]), (0,0,255), 3)
-
-
-			#----------------------------------------------------------------
-			#----------Velocity/Danger Area (Danger Check #2)----------------
-			#----------------------------------------------------------------
-
-			#Finding Contours in black and white image
-			contours, hierarchy = cv2.findContours(thresh3, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)   #NONE
-
-			#Initiating Hull Array
-			hull = []
-
-			#Filling Convex Hull array with points
-
-			for i in range(len(contours)):
-				hull.append(cv2.convexHull(contours[i], False))
-
-			if len(contours) == 1:
-				leftHandPinBool = True
-			elif len(contours) >= 2:
-				leftHandPinBool = True
-				rightHandPinBool = True
-
-			for i in range(len(contours)):
-				#Drawing Contours
-				area = cv2.contourArea(contours[i])
-				if area < 500:
-					continue
-				cv2.drawContours(img, contours, i, (255,255,255), 1, 1, hierarchy)
-				#Drawing Hull
-				#cv2.drawContours(img, hull, i, (255,0,0), 3, 8)
-
-				for j in range(len(contours[i])):
-
-					if contours[i][j][0][1] > height - rectHeight - extraSizeToRecty:
-						if contours[i][j][0][0] > width/2 - rectWidth - extraSizeToRectx/2 and contours[i][j][0][0] < width/2 + rectWidth + extraSizeToRectx/2 :
-							inRect = True
-
-
-			#------------If the hand is in the Danger Rectangle, do action
-			if inRect:
-				color = (255,255,255)
-				dangerVelocity = True
-			else:
-				color = (255,255,255)
-
-
-			#Drawing Danger Rectangle
-			cv2.rectangle(img, (width/2-rectWidth - extraSizeToRectx/2 , height - rectHeight - extraSizeToRecty), (width/2+rectWidth + extraSizeToRectx/2 , height), color, 0)
-
-
-
-			#cv2.imshow("img", img)
-			large = cv2.resize(img, (0,0), fx=5, fy=5)
-			cv2.imshow("large", large)
-			#cv2.imshow("diff", diffSmall)
-
-			#Updating currentFrame to be LastFrame
-			frameArray[0] = frameArray[1]
-			frameArray.pop(1)
-
-			timeInitial = time.time()
-
-			if dangerDifference or dangerVelocity:
-				#print("Multi")
-				redLEDPinBool = True
-				greenLEDPinBool = False
-				dropGuardPinBool = True
-				makeSafePinBool = True
-
-				thread = Thread(target=playWarning)
-				thread.daemon = True
-				thread.start()
-
-			GPIO.output(makeSafePin, makeSafePinBool)
-			GPIO.output(dropGuardPin, dropGuardPinBool)
-
-			GPIO.output(redLEDPin, redLEDPinBool)
-			GPIO.output(greenLEDPin, greenLEDPinBool)
-			GPIO.output(yellowLEDPin, yellowLEDPinBool)
-			GPIO.output(leftHandPin, leftHandPinBool)
-			GPIO.output(rightHandPin, rightHandPinBool)
-
-
-			k = cv2.waitKey(45) & 0xff
-			if k == 27:
-				break
-
-			#Adding to Frame Counter
-			frameCounter += 1
-			frameTime += dt
-
-			previousId = frame_id
-
+				waitUntilPin(commsPin, True, .1)
 
 
 
@@ -382,6 +389,8 @@ def main():
 
 		#cap.release()
 		cv2.destroyAllWindows()
+
+
 
 main()
 
@@ -416,3 +425,14 @@ while True:
 	finally:
 		pass
 	break
+
+def checkPin(pinNum):
+	return GPIO.input(pinNum)
+
+def waitUntilPin(pinNum, desiredValue, reCheckInterval):
+	while True:
+		pinInput = checkPin(pinNum)
+		if pinInput == value:
+			yield pinInput
+		else:
+			time.sleep(reCheckInterval)
